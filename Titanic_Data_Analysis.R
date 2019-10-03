6fba687d5371d265c35842d48affbed9afa24410
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-10-03 21:46:31 +0200

    added the loading of train and test data

--- Titanic_Data_Analysis.R
+++ Titanic_Data_Analysis.R
@@ -1,2 +1,7 @@
 # Comments in R are prefixed the same way a sin Python
 # Test Data and Train Data for the Titanic competiotion
+
+# Load raw data for both test and train sets
+train <- read.csv("train.csv", header = TRUE)
+test <- read.csv("test.csv", header = TRUE)
+
